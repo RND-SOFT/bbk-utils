c940d9157a72e11b07f3321a1d8d7529eefa23d0
Samoilenko Yuri <kinnalru@gmail.com>
2020-10-09 09:16:04 +0300

    fix

--- spec/aggredator/utils/logger_spec.rb
+++ spec/aggredator/utils/logger_spec.rb
@@ -32,4 +32,8 @@ RSpec.describe Aggredator::Logger do
     logger.info('hello2')
     expect(output).to match(/#{tag}.*hello2/)
   end
+
+  it 'ActiveSupport::TaggedLogging' do
+    expect(described_class.new progname, nil, io: io).to be_an(ActiveSupport::TaggedLogging)
+  end
 end
