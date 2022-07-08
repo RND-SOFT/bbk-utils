13388e8dbb91fddba7ec495504731a85af5c0917
Ivan Godko <igodko@rnds.pro>
2022-07-08 14:23:33 +0300

    gracefully_main from another gem

--- lib/bbk/utils.rb
+++ lib/bbk/utils.rb
@@ -17,6 +17,24 @@ module BBK
 
       attr_accessor :logger
 
+      def gracefully_main
+        yield
+        0
+      rescue SignalException => e
+        if %w[INT TERM EXIT QUIT].include?(Signal.signame(e.signo))
+          0
+        else
+          logger.error "Signal: #{e.inspect}"
+          1
+        end
+      rescue StandardError => e
+        logger.error "Exception: #{e.inspect}. Backtrace: #{e.backtrace.inspect}"
+        1
+      rescue SystemExit => e
+        logger.error "System exit: #{e.inspect}. Backtrace: #{e.backtrace.inspect}"
+        e.status
+      end
+
     end
 
     self.logger = ::BBK::Utils::Logger.default
