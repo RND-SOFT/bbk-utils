141b9f6ca1978a0887cf0b04b03298db20f9f788
vkryukov <vkryukov@rnds.pro>
2026-08-07 15:09:28 +0300

    add filtering

--- lib/bbk/utils/log_formatter.rb
+++ lib/bbk/utils/log_formatter.rb
@@ -8,12 +8,14 @@ module BBK
 
       FORMAT = "%5s [%sUTC #%d] (%s)[%s]: %s\n"
 
-      def initialize(tags: [])
+      def initialize(tags: [], filter: nil)
         super()
         @tags = tags
+        @filter = filter
       end
 
       def call(severity, time, progname, msg)
+        msg = try_filtering(msg)
         line = msg2str(msg).gsub("\n", '\\n')
         line = "#{build_tags_text}#{line}"
         format(FORMAT, severity, format_datetime(time.utc), Process.pid, progname, thread_id, line)
@@ -32,11 +34,24 @@ module BBK
         end
       end
 
-      private def build_tags_text
+      private
+
+      def build_tags_text
         @tags.collect { "[#{_1}] " }.join if @tags.any?
+      end
+
+      def try_filtering(msg)
+        return msg if @filter.nil?
+
+        if msg.is_a?(Hash)
+          @filter.filter(msg)
+        elsif msg.respond_to?(:to_h)
+          @filter.filter(msg.to_h)
+        else
+          msg
+        end
       end
 
     end
   end
 end
-
--- spec/bbk/utils/log_formatter_spec.rb
+++ spec/bbk/utils/log_formatter_spec.rb
@@ -1,17 +1,18 @@
 RSpec.describe BBK::Utils::LogFormatter do
-  subject { described_class.new }
+  subject { described_class.new(filter:) }
 
   let(:progname) { SecureRandom.hex }
   let(:severity) { Logger::Severity.constants.map(&:to_s).sample }
   let(:time) { Time.now - Random.rand(1..42).minutes }
   let(:message) { SecureRandom.hex }
-
+  let(:filter) { nil }
 
   it 'call' do
     expect(subject).to receive(:msg2str).with(message).and_call_original
     expect(subject).to receive(:format_datetime).with(time.utc)
     expect(subject).to receive(:format).with(described_class::FORMAT, severity, anything, $PROCESS_ID, progname,
                                              any_args).and_call_original
+    expect(subject).to receive(:try_filtering).with(message).and_call_original
     subject.call(severity, time, progname, message)
   end
 
@@ -24,5 +25,47 @@ RSpec.describe BBK::Utils::LogFormatter do
     expect(parts.first).to eq Thread.current.object_id.to_s
     expect(parts.last).to eq Thread.current.name
   end
-end
 
+  describe 'with filter' do
+    let(:filter) { double("Filter", filter: message)}
+
+    context 'with simple message' do
+      it 'call' do
+        expect(filter).not_to receive(:filter)
+        subject.call(severity, time, progname, message)
+      end
+    end
+
+    context 'with hash message' do
+      let(:message) { { test: SecureRandom.hex} }
+
+      it 'call' do
+        expect(filter).to receive(:filter).with(message).and_return(message)
+        subject.call(severity, time, progname, message)
+      end
+    end
+
+    context 'with object responding to to_h' do
+      let(:message) do
+        Struct.new(:test).new(SecureRandom.hex)
+      end
+
+      it 'call' do
+        expect(filter).to receive(:filter).with(message.to_h).and_return(message.to_h)
+
+        subject.call(severity, time, progname, message)
+      end
+    end
+
+    context 'with string' do
+      let(:message) { '42' }
+
+      it 'call' do
+        expect(filter).not_to receive(:filter)
+
+        subject.call(severity, time, progname, message)
+      end
+    end
+  end
+
+end
